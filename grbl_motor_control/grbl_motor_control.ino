9871d0d47443848a844d313c7d3fb7bed246ddb3
omrinolansupersharp <omri.nolan@supersharp.space>
2025-04-03 16:55:50 +0100

    Update grbl_motor_control.ino

--- grbl_motor_control/grbl_motor_control.ino
+++ grbl_motor_control/grbl_motor_control.ino
@@ -11,7 +11,7 @@ E.g. to do 1 turn on all motors in 1 minute: G1X0.8Y0.8Z0.8F0.8
 E.g. to do 1 turn on 1 motor in 1 minute: G1X0.8F0.8
 
 Limits on F speed when moving 1 motor:
-max speed 200
+max speed 300
 min speed 1 (anything less than this moves at the same velocity)
 
 Motion times for F commands:(G1 X0 Y0.8 Z0 F(a))
@@ -33,9 +33,17 @@ G1X0.8Y0Z0F20 - turns the x motor one revolution in the positive direction in 3.
 G1X-0.8Y0Z0F20 - turns the x motor one revolution in the positive direction in 3.2 seconds
 
 #0.004 is one step
-#0.8 is one full turn of the motor
+#0.8 (mm) is one full turn of the motor
 #1:50 is the ratio for the worm gear 
 #0.25mm is the thread
+#ratio between motor turns and height of actuator is:
+actuator height = motor distance  * gear ratio * thread pitch 
+actuator height = motor distance  * 0.02 * 0.25mm
+actuator height = motor distance * 0.005mm
+e.g. for 1mm motor movement:
+actuator height change = 0.004mm
+#200 steps in one full motor turn
+actuator height change per one step = 0.004 *0.005 = 0.00002mm = 20nm
 
 */
 #include <M5Stack.h>
@@ -43,6 +51,8 @@ G1X-0.8Y0Z0F20 - turns the x motor one revolution in the positive direction in 3
 #include "ClosedCube_TCA9548A.h"
 #include "M5Module4EncoderMotor.h"
 #include "Unit_RTC.h"
+#include <Preferences.h>
+Preferences preferences;
 
 
 #define PaHub_I2C_ADDRESS (0x77)
@@ -89,11 +99,24 @@ uint8_t mod_address_E = 0x5A;
 Unit_RTC RTC;
 rtc_time_type RTCtime;
 rtc_date_type RTCdate;
+unsigned long previousMillis = 0;
+const long interval = 10; // Interval to check motor status (in milliseconds)
+unsigned long timeout = 5000; // Timeout period (in milliseconds)
+unsigned long startTime = millis();
 
 
 int petal = 0; // startup petal is 0
 int32_t encoder_readings[5][3] = {{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}};
-int32_t pos[5][3] = {{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}};
+float pos[5][3];
+float initialPos[5][3] = {
+    {0.000000, 0.000000, 0.000000},
+    {0.000000, 0.000000, 0.000000},
+    {0.000000, 0.000000, 0.000000},
+    {0.000000, 0.000000, 0.000000},
+    {0.000000, 0.000000, 0.000000}
+};
+float conv = 0.004; // conversion between GCode X1 and distance moved by screw
+int wait = 300; //scren refresh time in ms
 
 void setup() {
     M5.begin();
@@ -111,13 +134,9 @@ void setup() {
     M5.Lcd.print(petal);
     M5.Lcd.setCursor(20, 110); 
     M5.Lcd.print("Encoder Positions are: ");
-    M5.Lcd.setCursor(20, 140);  
-    M5.Lcd.print(pos[petal][0]);
-    M5.Lcd.setCursor(100, 140);
-    M5.Lcd.print(pos[petal][1]);
-    M5.Lcd.setCursor(180, 140);
-    M5.Lcd.print(pos[petal][2]);
 
+    savepos(initialPos);
+    readpos(pos);
 
     Wire.begin(21, 22);
     tca9548a.address(PaHub_I2C_ADDRESS);  // Set the I2C address
@@ -147,24 +166,31 @@ void setup() {
 }
 
 void loop() {
-    M5.update();
+    update();
+
     String cmd = ""; // Declare cmd at the beginning of the loop
     if (Serial.available()) {
         // Read the serial input
         Serial.println("");
-        cmd = Serial.readStringUntil('\n');       
-    }
+        cmd = Serial.readStringUntil('\n');
+        Serial.println(cmd); 
+        delay(10);      
+    
 
     // changing petal number
     if (cmd[0] == 'P'){
       petal = cmd[1]-'0';
+      Serial.print("Active Petal changed to: ");
+      Serial.println(petal);
     }
 
     // Query position
     if (cmd[0] == 'Q'){
+      Serial.println("The current position is: ");
       for (int i = 0; i < 5; ++i) {
         for (int j = 0; j < 3; ++j) {
             Serial.print(pos[i][j]);
+            Serial.print(",");
         }
       }
       Serial.println(" ");
@@ -193,8 +219,13 @@ void loop() {
     // Checking if Idle
     if (cmd[0] == 'I'){
       int  petal_cmd = cmd[1]-'0';
-      bool isIdle = petalMap[petal_cmd].motor->readIdle();
-      Serial.println(isIdle);
+      if (petal_cmd >= 0 && petal_cmd < 5) {
+            bool isIdle = petalMap[petal_cmd].motor->readIdle();
+            Serial.print("Motor idle status: ");
+            Serial.println(isIdle); // Debugging statement
+        } else {
+            Serial.println("Invalid petal command"); // Debugging statement
+        }
     }
 
     // reading encoders - need to save until we actually have encoders
@@ -208,24 +239,72 @@ void loop() {
     if (cmd[0] == 'G'){ // move motor command
       //bool 
       move_all_motors(cmd,petal);
+      Serial.println("Motion finished");
     }
-    String data = Serial.readString();
-    Serial.println(data);
+
+    if (cmd[0] == 'M'){ // move motor command
+      //bool 
+      //set_all_motors(,petal);
+    }
+    if (cmd[0] == 'V'){ // Save positions to NonVolatileMemory
+      savepos(pos);
+    }
+    
+    
+}   
+}
+// END OF LOOP
+
+// FUUNCTIONS
+
+void move_all_motors(String command, int petal) {
+    // Convert the command into a char array
+    char buffer[command.length() + 1];
+    command.toCharArray(buffer, sizeof(buffer));
+    Serial.print("Command buffer: ");
+    Serial.println(buffer); // Debugging statement
+    // extract the position variable from the G command
+    float x = extractValue(command, 'X');
+    float y = extractValue(command, 'Y');
+    float z = extractValue(command, 'Z');
+    int f = extractValue(command, 'F');
+    // Select the correct PAHub address for the selected petal
+    tca9548a.selectChannel(petalMap[petal].pahub_address);
+    Serial.print("Selected PAHub channel: ");
+    Serial.println(petalMap[petal].pahub_address); // Debugging statement
+
+    // Check if the motor is connected by calling the isMotorConnected function
+    if (isMotorConnected(petalMap[petal].motor)) {
+        // Send the command to the selected petal motor
+        petalMap[petal].motor->sendGcode(buffer);
+        Serial.println("G-code command sent"); // Debugging statement
+        // Wait for motor to become idle before finishing move
+        while (!petalMap[petal].motor->readIdle()) {
+          unsigned long currentMillis = millis();        
+          if (currentMillis - previousMillis >= interval) {
+            previousMillis = currentMillis;
+            Serial.println("Motor is not idle, waiting..."); // Debugging statement
+          }  
+          // Check if timeout period has elapsed
+          if (currentMillis - startTime >= (timeout*30*(x+y+z)/f)){
+            Serial.println("Timeout reached, exiting loop.");
+            break;
+          }
+        }
+        Serial.println("Motor is idle again after move"); // Debugging statement
+    } else {
+        Serial.println("Motor is not connected"); // Debugging statement
+    }
+    
+    //update pos variable
+    pos[petal][0] += (x * conv);
+    pos[petal][1] += (y * conv);
+    pos[petal][2] += (z * conv);
+    Serial.println(pos[petal][0]);
     
 }
 
-void move_all_motors(String command, int petal) {
-  char buffer[command.length() + 1];
-  command.toCharArray(buffer, sizeof(buffer));
-  tca9548a.selectChannel(petalMap[petal].pahub_address);
-  petalMap[petal].motor->sendGcode(buffer);
-
-  //wait for motor to become idle before finishing move
-  while (!petalMap[petal].motor->readIdle()) {
-  delay(10);
-  }
-}
-
+/*
 void set_all_motors(int x = 0, int y = 0, int z = 0, int speed = 300, int petal = 0) {
   tca9548a.selectChannel(petalMap[petal].pahub_address);
   petalMap[petal].motor->setMotor(x,y,z,speed);
@@ -238,6 +317,7 @@ void set_all_motors(int x = 0, int y = 0, int z = 0, int speed = 300, int petal 
   pos[petal][1] += y;
   pos[petal][2] += z;
 }
+*/
 
 int32_t GetEncoderValue(int petal, int motor){ // get encoder value
   if (petal == 0){
@@ -257,3 +337,74 @@ int32_t GetEncoderValue(int petal, int motor){ // get encoder value
   }
     return encoder_readings[petal][motor];
 }
+
+bool isMotorConnected(Module_GRBL* motor) { // this is currently not working - need to figure out what is returned by the status query
+    const int maxAttempts = 3;
+    for (int attempt = 0; attempt < maxAttempts; ++attempt) {
+        // Send a simple command to check connection
+        char checkCommand[] = "?";
+        motor->sendGcode(checkCommand);
+        delay(100); // Wait for response
+
+        // Read the response from the motor
+        String response = motor->readLine();
+        Serial.print("Motor response: ");
+        Serial.println(response); // Debugging statement
+
+        // Check if the response contains "ok" and does not contain corrupted data
+        if (response.length() > 0 && response.indexOf("�") == -1 ) { //&& response.indexOf("ok") != -1
+            return true; // Valid response indicates a connection
+        }
+    }
+    return false; // After maxAttempts, conclude the motor is not connected
+}
+
+float extractValue(String command, char axis) {
+  int startIndex = command.indexOf(axis);
+  if (startIndex == -1) {
+    return 0.0; // Return 0 if the axis is not found
+  }
+  int endIndex = command.indexOf(' ', startIndex);
+  if (endIndex == -1) {
+    endIndex = command.length();
+  }
+  String valueStr = command.substring(startIndex + 1, endIndex);
+  return valueStr.toFloat();
+}
+
+void savepos(float pos[5][3]) {
+  // Store modified array in NVS
+  preferences.begin("my-app", false); // Initialize preferences with read/write mode
+  preferences.putBytes("Actpos", (float*)pos, sizeof(float) * 5 * 3);
+  preferences.end(); // Close preferences
+}
+
+void readpos(float pos[5][3]) {
+  // Read array from NVS
+  preferences.begin("my-app", true); // Initialize preferences with read-only mode
+  preferences.getBytes("Actpos", (float*)pos, sizeof(float) * 5 * 3);
+  preferences.end(); // Close preferences
+}
+
+void update(){
+  delay(wait);
+  // Clear the area where the values are printed
+  M5.Lcd.fillRect(20, 80, 200, 20, BLACK); // Clear petal value
+  M5.Lcd.fillRect(20, 140, 60, 20, BLACK); // Clear pos[petal][0]
+  M5.Lcd.fillRect(100, 140, 60, 20, BLACK); // Clear pos[petal][1]
+  M5.Lcd.fillRect(180, 140, 60, 20, BLACK); // Clear pos[petal][2]
+
+  // Print updated values
+  M5.Lcd.setCursor(20, 80);  
+  M5.Lcd.print(petal);
+  M5.Lcd.setCursor(20, 140);  
+  M5.Lcd.print(pos[petal][0]);
+  M5.Lcd.setCursor(100, 140);
+  M5.Lcd.print(pos[petal][1]);
+  M5.Lcd.setCursor(180, 140);
+  M5.Lcd.print(pos[petal][2]);
+
+}
+
+
+
